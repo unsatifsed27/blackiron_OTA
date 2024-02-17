a0fbe75287bab524195add6dcc52f801e20bc626
AidanWarner97 <aidan@awarner.co.uk>
2024-02-17 19:24:52 +0000

    ota.sh: Readd support for datetime, and update variables

--- support/ota.sh
+++ support/ota.sh
@@ -4,21 +4,23 @@ DEVICE_NAME=$3
 XDA_THREAD=$4
 GHUN=$5
 NAME=$6
+DEVGRP=$7
 
+DATETIME=$(grep "ro.build.date.utc=" out/target/product/$DEVICE/system/build.prop | cut -d "=" -f 2)
 FILENAME=$(find out/target/product/$DEVICE/Blackiron*.zip | cut -d "/" -f 5)
 ID=$(sha256sum out/target/product/$DEVICE/Blackiron*.zip | cut -d " " -f 1)
 FILEHASH=$(md5sum out/target/product/$DEVICE/Blackiron*.zip | cut -d " " -f 1)
 SIZE=$(wc -c out/target/product/$DEVICE/Blackiron*.zip | awk '{print $1}')
 URL="https://sourceforge.net/projects/black-iron-project/files/$DEVICE/$FILENAME/download"
-VERSION=$(grep "org.blki.version=" out/target/product/$DEVICE/system/build.prop | cut -d "=" -f 2)
+VERSION=$(grep "ro.blackiron.build.version=" out/target/product/$DEVICE/system/build.prop | cut -d "=" -f 2)
 STATUS="Active"
-BLKIV=$(grep "org.blki.version=" out/target/product/$DEVICE/system/build.prop | cut -d "=" -f 2)
+BLKIV=$(grep "ro.blackiron.version=" out/target/product/$DEVICE/system/build.prop | cut -d'=' -f2 | cut -d'_' -f1)
 DONATE_URL="http://www.paypal.me/ralf979"
 WEBSITE_URL="https://lol.lol/"
 NEWS_URL="https://t.me/BlkiUpdate"
 JSON_FMT='{\n\t"error":false,\n\t"filename":"%s",\n\t"datetime":%s,\n\t"size":%s,\n\t"url":"%s",\n\t"filehash":"%s",\n\t"version":"%s",\n\t"status":"%s",\n\t"blkiv":"%s",\n\t"id":"%s",\n\t"tg_username":"%s",\n\t"device_name":"%s",\n\t"device":"%s",\n\t"xda_thread":"%s",\n\t"maintainers": [{\n\t\t"main_maintainer":false,\n\t\t"github_username":"%s",\n\t\t"name":"%s"\n\t}],\n\t"donate_url":"%s",\n\t"website_url":"%s",\n\t"news_url":"%s",\n\t"forum_url":"%s"\n}'
 
-printf "$JSON_FMT" "$FILENAME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$STATUS" "$BLKIV" "$ID" "$TG_USERNAME" "$DEVICE_NAME" "$DEVICE" "$XDA_THREAD" "$GHUN" "$NAME" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$XDA_THREAD" > OTA/builds/$DEVICE.json
+printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$STATUS" "$BLKIV" "$ID" "$TG_USERNAME" "$DEVICE_NAME" "$DEVICE" "$XDA_THREAD" "$GHUN" "$NAME" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$XDA_THREAD" "$DEVGRP" > OTA/builds/$DEVICE.json
 echo OTA/builds/$DEVICE.json file created
 
 if [[ $DEVICE == *"_"* ]]; then
