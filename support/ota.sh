b7d7889a7df3e6e2b2fab8dfe9efcfe237adbb2c
AidanWarner97 <aidan@awarner.co.uk>
2024-02-25 16:26:23 +0000

    ota.sh: Try to include Device group link again
    Signed-off-by: AidanWarner97 <aidan@awarner.co.uk>

--- support/ota.sh
+++ support/ota.sh
@@ -19,6 +19,7 @@ DONATE_URL="http://www.paypal.me/ralf979"
 WEBSITE_URL="https://lol.lol/"
 NEWS_URL="https://t.me/BlkiUpdate"
 JSON_FMT='{\n\t"error":false,\n\t"filename":"%s",\n\t"datetime":%s,\n\t"size":%s,\n\t"url":"%s",\n\t"filehash":"%s",\n\t"version":"%s",\n\t"status":"%s",\n\t"blkiv":"%s",\n\t"id":"%s",\n\t"tg_username":"%s",\n\t"device_name":"%s",\n\t"device":"%s",\n\t"xda_thread":"%s",\n\t"maintainers": [{\n\t\t"main_maintainer":false,\n\t\t"github_username":"%s",\n\t\t"name":"%s"\n\t}],\n\t"donate_url":"%s",\n\t"website_url":"%s",\n\t"news_url":"%s",\n\t"forum_url":"%s"\n}'
+DEV_GRP=$DEVICE
 
 printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$STATUS" "$BLKIV" "$ID" "$TG_USERNAME" "$DEVICE_NAME" "$DEVICE" "$XDA_THREAD" "$GHUN" "$NAME" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$XDA_THREAD" "$DEVGRP" > OTA/builds/$DEVICE.json
 echo OTA/builds/$DEVICE.json file created
